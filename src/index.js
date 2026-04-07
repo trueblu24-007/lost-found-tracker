5bb26f8b2340bf213b7c3fb1ded49d85f4b78dff
Parth Batra <parth.batra2024@vitstudent.ac.in>
2026-04-07 22:35:53 +0530

    Create index.js for React entry point

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Entry point of the React application
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
